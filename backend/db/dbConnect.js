01bb62b3ac4ac839a7357b77aa23edee130c3d4f
zark28 <adamsabdulrazak28@gmail.com>
2023-04-09 00:41:00 +0000

    change database connection to mySQL

--- backend/db/dbConnect.js
+++ backend/db/dbConnect.js
@@ -1,21 +1,20 @@
+const mysql = require("mysql")
 // db connection
-const sqlite3 = require("sqlite3").verbose()
-
-
-let db= new sqlite3.Database("./db/companydb",sqlite3.OPEN_READWRITE, (err)=>{
-    if(err){
-        console.log(`error occured ${err.message}`);
-    }else{
-        console.log("Database connected Successfully");
-    }
+const connection = mysql.createConnection({
+    host:"",
+    user:"root",
+    password:"",
+    database:"food_clock"
 })
+
+const dbOpen=()=>{
+     // database open
+    connection.connect(console.log("Database connected Successfully"))
+}
 
 const dbClose=()=>{
     // database close
-database.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
+connection.end(console.log('Closed the database connection.'))
 }
+
+module.exports= {connection,dbOpen,dbClose}
--- backend/server.js
+++ backend/server.js
@@ -1,82 +1,59 @@
 const express =require("express")
-const sqlite3 = require("sqlite3").verbose()
 const cors =require("cors")
 const app =express()
 app.use(cors())
 // setting dynamic port
 const PORT = process.env.PORT||3000
 
-// db connection
-let db= new sqlite3.Database("./db/foodclock",sqlite3.OPEN_READWRITE, (err)=>{
-    if(err){
-        console.log(`error occured ${err.message}`);
-    }else{
-        console.log("Database connected Successfully");
-    }
-})
+require("./db/dbConnect")
 
-const dbClose=()=>{
-    // database close
-database.close((err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
-}
-
-
-app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded());
+app.set('view engine', 'ejs');
 
-app.get("/", function(req,res){
-  res.render("index",{name:"Steven"})   
-});
+// app.get("/", function(req,res){
+//   res.render("index",{name:"Steven"})   
+// });
 
-app.post("/login",(req,res)=>{
-    const {email,password}=req.body
-    db.serialize(() => {
-            db.run(`SELECT *
-                    FROM users 
-                    WHERE users.email = "${email}" and users.password = "${password}"`, (err, result) => {
-            if (err) {
-                console.error(err.message);
-            }
-            res.render("home",{data:result})
-            console.log(result);
-            });
-        });
-})
+// app.post("/login",(req,res)=>{
+//     const {email,password}=req.body
 
-app.post("/signup",(req,res)=>{
-    const {email,password}=req.body
-    db.serialize(() => {          
-        //         db.run(`SELECT * FROM users 
-        //         WHERE users.email = "${email}"`, (err, result) => {
-        //             if (err) {
-        //             console.error(err.message);
+//     connection.query(`SELECT * FROM users 
+//         WHERE users.email = "${email}" and users.password = "${password}"`, 
+//         (err, result) => {
+//             if (err) {
+//                 console.error(err.message);
+//             }
 
-        //             if(result.email){
-        //                 alert('user already exits')
-        //                     return
-        //             }
-        //            }
-        // })  
-      
-            db.run(`INSERT INTO users  VALUES("${email}", "${password}", NULL, 'user') `, (err, result) => {            
-                if(err){
-                    console.log(err);
-                }
-                res.render("home",{data:{email:this.changes,password:password}})
-            })  
+//             if(result.email === email){
+//                 db.each(
+//                     `SELECT * 
+//                     FROM meanu `
+//                 )
+//                 // res.render("home",{data:result})
+//             }
+//             console.log(result);
+//             });  
         
-        
-                            
-    });    
-});
+// })
+
+// app.post("/signup",(req,res)=>{
+//     const {email,password,role}=req.body         
+//             connection.query(`INSERT INTO users  VALUES( "${NULL}","${email}", "${password}", "${role}") `, (err, result) => {            
+//                 if(err){
+//                     console.log(err);
+//                 }
+//                 console.log(result);
+//                 // res.render("home",{data:{email:email,roles:"user"}})
+//             })  
+// });
 
 
 
-app.listen(PORT);
-console.log(`listening on http://localhost:${PORT}`);
+
+// app.use("/*",(req,res)=>{
+//     res.render("error")
+// })
+
+
+
+app.listen(PORT,()=>console.log(`listening on http://localhost:${PORT}`))

83d2c97bf75c44da6760cee44ce3090651c9df28
Calvin <Calvinasty9@gmail.com>
2023-05-14 17:16:27 +0000

    logo format change

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,13 +1,13 @@
 <template>
     <main class="landing-page flex-center">
-        
-        <img class="home-logo" src="/images/homeLogo.svg" alt="food-clock logo">
+
+        <img class="home-logo" src="/images/homeLogo.png" alt="food-clock logo">
 
 
-            <router-link to="/signup">
-                <button class="btn home-btn">Get Started</button>
-            </router-link> 
-        
+        <router-link to="/signup">
+            <button class="btn home-btn">Get Started</button>
+        </router-link>
+
 
         <img class="banner" src="/images/bg-img.svg" alt="banner">
 
@@ -15,31 +15,31 @@
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
 <style lang="css" scoped>
-.landing-page{
+.landing-page {
     flex-direction: column;
     justify-content: space-around;
     height: 100%;
     width: 100%;
 }
-.home-logo{
+
+.home-logo {
     height: 160px;
-    /* box-shadow: 0px 4px 20px rgba(101, 101, 101, 0.15); */ 
-}
-.home-btn{
-width:190px;
+    /* box-shadow: 0px 4px 20px rgba(101, 101, 101, 0.15); */
 }
 
-
-.banner{
-width: 276px;
-height: 251px;
+.home-btn {
+    width: 190px;
 }
 
 
+.banner {
+    width: 276px;
+    height: 251px;
+}
 </style>
